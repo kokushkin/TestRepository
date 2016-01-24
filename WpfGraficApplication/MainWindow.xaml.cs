381e7dc5df221c8d567795c7cb9b273f812fd18d
kokushkin <fipsiksi@yandex.ru>
2016-01-25 01:33:40 +0300

    add grafic window and user control

---
+++ WpfGraficApplication/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGraficApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Point[] PointArray { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PointArray = new Point[4];
+            PointArray[0] = new Point(0, 0);
+            PointArray[1] = new Point(20, 20);
+            PointArray[2] = new Point(64, 30);
+            PointArray[3] = new Point(100, 200);
+            Polyline polyLine = new Polyline();
+            polyLine.Stroke =new SolidColorBrush(Color.FromRgb(255,0,0));
+            polyLine.StrokeThickness = 2;
+            PointCollection pColl = new PointCollection(PointArray);
+            polyLine.Points = pColl;
+            R2.Children.Add(polyLine);
+            
+        }
+    }
+}
