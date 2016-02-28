fe48bfbbf1dbebde2dacb2e30762d5111b48d111
kokushkin <fipsiksi@yandex.ru>
2016-02-28 18:46:54 +0300

    add lines frome code

--- WpfTestGrafic/MainWindow.xaml.cs
+++ WpfTestGrafic/MainWindow.xaml.cs
@@ -44,6 +44,32 @@ namespace WpfTestGrafic
             OffsetY = -40;
 
             InitializeComponent();
+
+            PathFigure fig1 = new PathFigure();
+            fig1.StartPoint = new Point(500, 400);
+            fig1.Segments.Add(new LineSegment(new Point(40, 40), true));
+            PathGeometry pthGeom1 = new PathGeometry();
+            pthGeom1.Figures.Add(fig1);
+            pthGeom1.Transform = (Transform)grid.FindResource("transform");
+            Path blackPath = new Path();
+            blackPath.Stroke = new SolidColorBrush(Colors.Black);
+            blackPath.StrokeThickness = 4;
+            blackPath.Data = pthGeom1;
+            grid.Children.Add(blackPath);
+
+            PathFigure fig2 = new PathFigure();
+            fig2.StartPoint = new Point(100, 100);
+            fig2.Segments.Add(new LineSegment(new Point(300, 600), true));
+            PathGeometry pthGeom2 = new PathGeometry();
+            pthGeom2.Figures.Add(fig2);
+            pthGeom2.Transform = (Transform)grid.FindResource("transform");
+            Path greenPath = new Path();
+            greenPath.Stroke = new SolidColorBrush(Colors.Green);
+            greenPath.StrokeThickness = 4;
+            greenPath.Data = pthGeom2;
+            grid.Children.Add(greenPath);
+
         }
+
     }
 }
