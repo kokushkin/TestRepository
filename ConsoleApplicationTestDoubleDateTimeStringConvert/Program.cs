74464a894ee825a4f1d740a0d414fbb5047b061e
kokushkin <fipsiksi@yandex.ru>
2016-03-11 20:17:32 +0300

    interm commit

---
+++ ConsoleApplicationTestDoubleDateTimeStringConvert/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTestDoubleDateTimeStringConvert
+{
+    public class DoubleStringDateTimeConverter
+    {
+        public  string ToString(double value)
+        {
+            if (double.IsNegativeInfinity(value))
+                return "-∞";
+            else if (double.IsPositiveInfinity(value))
+                return "∞";
+            else if (double.IsNaN(value))
+                return "NaN";
+            else
+            {
+                DateTime dt = DateTime.FromOADate(value);
+                return dt.ToString("yyyy.MM.dd HH:mm:ss.fff");
+            }
+        }
+        public  double ToDouble(string value)
+        {
+            double res;
+            switch (value)
+            {
+                case "-∞":
+                    res = double.NegativeInfinity;
+                    break;
+                case "∞":
+                    res = double.PositiveInfinity;
+                    break;
+                case "NaN":
+                    res = double.NaN;
+                    break;
+                default:
+                    DateTime prsDt = DateTime.Parse(value);
+                    res = prsDt.ToOADate();
+                    break;
+            }
+            return res;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleStringDateTimeConverter cnv = new DoubleStringDateTimeConverter();
+            //DateTime dt = DateTime.UtcNow;
+            //string str = cnv.ToString(dt.ToOADate());
+            //Console.WriteLine(str);
+            //double dbl = cnv.ToDouble(str);
+            //Console.WriteLine(dbl);
+            //Console.WriteLine(cnv.ToString(dbl));
+
+            double pinfDouble = double.PositiveInfinity;
+            string str = cnv.ToString(pinfDouble);
+            double nanDouble = double.NaN;
+            string str2 = cnv.ToString(nanDouble);
+            double ninfDouble = double.NegativeInfinity;
+            string str3 = cnv.ToString(ninfDouble);
+
+            DateTime dtpinf = DateTime.FromOADate(pinfDouble);
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
--- WpfTestGrafic/SpecialConverter.cs
+++ WpfTestGrafic/SpecialConverter.cs
@@ -9,25 +9,48 @@ namespace WpfTestGrafic
 {
     public abstract class DoubleStringConverter
     {
-        string ToString(double value);
-        double ToDouble(string value);
+        abstract public string ToString(double value);
+        abstract public double ToDouble(string value);
     }
 
     public class DoubleStringDateTimeConverter : DoubleStringConverter
     {
         public override string ToString(double value)
         {
-            DateTime dt = (new DateTime(1970, 1, 1)).AddSeconds(Math.Floor(value)).AddMilliseconds((value - Math.Floor(value)) * 1000);
-            return dt.ToLongTimeString();
-            //DateTime dt = new DateTime(Convert.ToInt32(value));
-            //return String.Format(dt.ToString() + " {0}:{1}:{2}.{3}", dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+            if (double.IsNegativeInfinity(value))
+                return "-∞";
+            else if (double.IsPositiveInfinity(value))
+                return "∞";
+            else if (double.IsNaN(value))
+                return "NaN";
+            else
+            {
+                DateTime dt = DateTime.FromOADate(value);
+                return dt.ToString("yyyy.MM.dd HH:mm:ss.fff");
+            }
         }
         public override double ToDouble(string value)
         {
-            DateTime dt = default(DateTime);
-            DateTime.TryParse(value, out dt);
-            DateTime nwDatetime = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
-            return (nwDatetime - new DateTime(1970, 1, 1)).TotalSeconds + dt.Second / 1000;
+            double res;
+            switch(value)
+            {
+                case "-∞":
+                    res = double.NegativeInfinity;
+                    break;
+                case "∞":
+                    res = double.PositiveInfinity;
+                    break;
+                case "NaN":
+                    res = double.NaN;
+                    break;
+                default:
+                    DateTime prsDt = DateTime.Parse(value);
+                    res =  prsDt.ToOADate();
+                    break;
+            }
+            return res;
+
+            
         }
     }
 
@@ -37,25 +60,15 @@ namespace WpfTestGrafic
         public object Convert(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            // Возвращаем строку в формате 123.456.789 руб.
-            return ((double)value).ToString("#,###", culture) + " руб.";
+            DoubleStringConverter cnv = (DoubleStringConverter)parameter;
+            return cnv.ToString((double)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            double result;
-            if (Double.TryParse(value.ToString(), System.Globalization.NumberStyles.Any,
-                         culture, out result))
-            {
-                return result;
-            }
-            else if (Double.TryParse(value.ToString().Replace(" руб.", ""), System.Globalization.NumberStyles.Any,
-                         culture, out result))
-            {
-                return result;
-            }
-            return value;
+            DoubleStringConverter cnv = (DoubleStringConverter)parameter;
+            return cnv.ToDouble((string)value);
         }
     }
 }
