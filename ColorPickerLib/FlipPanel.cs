3f9b57acb4082cd27dab5ef04b4d82b58c832d50
kokushkin <fipsiksi@yandex.ru>
2016-06-20 10:08:25 +0300

    18_7

---
+++ ColorPickerLib/FlipPanel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace ColorPickerLib
+{
+    [TemplatePart(Name = "FlipButton", Type = typeof(ToggleButton)),
+    TemplatePart(Name = "FlipButtonAlternate", Type = typeof(ToggleButton)),
+    TemplateVisualState(Name = "Normal", GroupName = "ViewStates"),
+    TemplateVisualState(Name = "Flipped", GroupName = "ViewStates")]
+    public partial class FlipPanel : Control
+    {
+
+
+        //front panel
+        public static readonly DependencyProperty FrontContentProperty =
+     DependencyProperty.Register("FrontContent", typeof(object), typeof(FlipPanel), null);
+
+        public object FrontContent
+        {
+              get
+              {
+                     return GetValue(FrontContentProperty);
+              }
+              set
+              {
+                    SetValue(FrontContentProperty, value);
+              }
+        }
+
+        public static readonly DependencyProperty BackContentProperty =
+            DependencyProperty.Register("BackContent", typeof(object), typeof(FlipPanel), null);
+
+        public object BackContent
+        {
+            get
+            {
+                return GetValue(BackContentProperty);
+            }
+            set
+            {
+                 SetValue(BackContentProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty IsFlippedProperty =
+            DependencyProperty.Register("IsFlipped", typeof(bool), typeof(FlipPanel), null);
+
+        public bool IsFlipped
+        {
+            get
+            {
+                return (bool)GetValue(IsFlippedProperty);
+            }
+            set
+            {
+                SetValue(IsFlippedProperty, value);
+
+                ChangeVisualState(true);
+            }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(FlipPanel), null);
+
+        public CornerRadius CornerRadius
+        {
+            get
+            {
+                return (CornerRadius)GetValue(CornerRadiusProperty);
+            }
+            set
+            {
+                SetValue(CornerRadiusProperty, value);
+            }
+        }
+
+        public FlipPanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlipPanel),
+     new FrameworkPropertyMetadata(typeof(FlipPanel)));
+        }
+
+        static FlipPanel()
+        {
+
+        }
+    }
+}
