0b1156c30caf0d42e5115f60d3e43e704dcbff8e
NK <fipsiksi@yandex.ru>
2016-07-07 12:16:21 +0300

    next commit

---
+++ CloneCovariantProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneCovariantProject
+{
+    public interface IClonable<out T> : IMyInterface
+    {
+        T ShallowClone();
+    }
+
+    public interface IMyInterface
+    {
+        void DoSomething();
+    }
+
+    public class MyClass : IMyInterface
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("I do something.");
+        }
+    }
+
+    public class MyClassCloneble<T> : IClonable<T>, IMyInterface
+    {
+
+        public T ShallowClone()
+        {
+            return (T)this.ShallowClone();
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IClonable<IMyInterface> ref1 = new MyClassCloneble<MyClass>();
+
+        }
+    }
+}
