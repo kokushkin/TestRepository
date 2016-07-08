069255a60d73acb70405159aed8f90864d67c013
NK <fipsiksi@yandex.ru>
2016-07-08 20:59:18 +0300

    next commit

---
+++ ProjectBilling.MVVM/BillingProject.Model.cs
@@ -0,0 +1,67 @@
+﻿using ProjectBilling.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBilling.Application
+{
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void NotifyPropertyChanged(
+            string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public interface IProjectsModel
+    {
+        ObservableCollection<Project> Projects { get; set; }
+        event EventHandler<ProjectEventArgs> ProjectUpdated;
+        void UpdateProject(IProject updatedProject);
+    }
+
+
+    public class ProjectEventArgs : EventArgs
+    {
+        public IProject Project { get; set; }
+        public ProjectEventArgs(IProject project)
+        {
+            Project = project;
+        }
+    }
+
+    public class ProjectsModel : IProjectsModel
+    {
+        public ObservableCollection<Project> Projects { get; set; }
+        public event EventHandler<ProjectEventArgs> ProjectUpdated = delegate { };
+
+        public ProjectsModel(IDataService dataService)
+        {
+            Projects = new ObservableCollection<Project>();
+            foreach (Project project in dataService.GetProjects())
+            {
+                Projects.Add(project);
+            }
+        }
+
+        public void UpdateProject(IProject updatedProject)
+        {
+            GetProject(updatedProject.ID).Update(updatedProject);
+            ProjectUpdated(this,
+                new ProjectEventArgs(updatedProject));
+        }
+
+        private Project GetProject(int projectId)
+        {
+            return Projects.FirstOrDefault(
+                project => project.ID == projectId);
+        }
+    }
+}
---
+++ ProjectBilling.MVVM/BillingProject.ProjectViewModel.cs
@@ -0,0 +1,104 @@
+﻿using ProjectBilling.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBilling.Application
+{
+        public interface IProjectViewModel : IProject
+        {
+            Status EstimateStatus { get; set; }
+        }
+
+         public class ProjectViewModel : Notifier, IProjectViewModel
+         {
+             private int _id;
+             private string _name;
+             private double _estimate;
+             private double _actual;
+             private Status _estimateStatus = Status.None;
+
+             public ProjectViewModel()
+             { }
+
+             public ProjectViewModel(IProject project)
+             {
+                 if (project == null)
+                     return;
+                 ID = project.ID;
+                 Update(project);
+             }
+
+             public int ID
+             {
+                 get { return _id; }
+                 set
+                 {
+                     _id = value;
+                     NotifyPropertyChanged("Id");
+                 }
+             }
+
+             public string Name
+             {
+                 get { return _name; }
+                 set
+                 {
+                     _name = value;
+                     NotifyPropertyChanged("Name");
+                 }
+             }
+
+             public double Estimate
+             {
+                 get { return _estimate; }
+                 set
+                 {
+                     _estimate = value;
+                     NotifyPropertyChanged("Estimate");
+                 }
+             }
+
+             public double Actual
+             {
+                 get { return _actual; }
+                 set
+                 {
+                     _actual = value;
+                     UpdateEstimateStatus();
+                     NotifyPropertyChanged("Actual");
+                 }
+             }
+
+             public Status EstimateStatus
+             {
+                 get { return _estimateStatus; }
+                 set
+                 {
+                     _estimateStatus = value;
+                     NotifyPropertyChanged("EstimateStatus");
+                 }
+             }
+
+             public void Update(IProject project)
+             {
+                 ID = project.ID;
+                 Name = project.Name;
+                 Estimate = project.Estimate;
+                 Actual = project.Actual;
+             }
+
+             private void UpdateEstimateStatus()
+             {
+                 if (Actual == 0)
+                     EstimateStatus = Status.None;
+                 else if (Actual <= Estimate)
+                     EstimateStatus = Status.Good;
+                 else
+                     EstimateStatus = Status.Bad;
+             }
+         }
+
+}
