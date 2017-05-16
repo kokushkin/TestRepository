24376215c486088146feb6d0fb35380096f950e7
NK <fipsiksi@yandex.ru>
2017-05-16 17:47:19 +0300

    два простых сервиса. правда не один не отвечает из браузера(400)

---
+++ WCFExamples/Part1/ProgrammService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace EssentialWCF
+{
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        double GetPrice(string ticker);
+    }
+    public class StockService : IStockService
+    {
+        public double GetPrice(string ticker)
+        {
+            return 94.85;
+        }
+    }
+    public class Service
+    {
+        public static void Main()
+        {
+            ServiceHost serviceHost = new ServiceHost((typeof(StockService)), new Uri("http://localhost:8000/EssentialWCF"));
+            serviceHost.AddServiceEndpoint(typeof(IStockService), new BasicHttpBinding(), "");
+            serviceHost.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n"); Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
