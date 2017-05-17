5359d2de12bb9438fc4b172f46ca8df5d23aeb3a
NK <fipsiksi@yandex.ru>
2017-05-17 12:22:30 +0300

    MEX есть, из браузера обычный метод не вызывается

---
+++ WCFExamples/MEXConfigService/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace EssentialWCF
+{
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        double GetPrice(string ticker);
+    }
+    public class StockService : IStockService
+    {
+        public double GetPrice(string ticker)
+        {
+            return 94.85;
+        }
+    }
+    public class Service
+    {
+        public static void Main()
+        {
+            ServiceHost serviceHost = new ServiceHost((typeof(StockService)));
+            serviceHost.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n"); Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
---
+++ WCFExamples/MEXProgrammService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace EssentialWCF
+{
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        double GetPrice(string ticker);
+    }
+    public class StockService : IStockService
+    {
+        public double GetPrice(string ticker)
+        {
+            return 94.85;
+        }
+    }
+    public class Service
+    {
+        public static void Main()
+        {
+            ServiceHost serviceHost = new ServiceHost((typeof(StockService)), new Uri("http://localhost:8000/EssentialWCF"));
+
+            serviceHost.AddServiceEndpoint(typeof(IStockService), new BasicHttpBinding(), "");
+
+            ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            behavior.HttpGetEnabled = true;
+            serviceHost.Description.Behaviors.Add(behavior);
+
+            serviceHost.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+
+            serviceHost.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n");
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
