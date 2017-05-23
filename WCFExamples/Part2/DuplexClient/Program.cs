7512fd771a6f5007ada6b3885c946e4592ee0948
NK <fipsiksi@yandex.ru>
2017-05-23 22:23:11 +0300

    клиент для дуплексной службы - ок

---
+++ WCFExamples/Part2/DuplexClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using DuplexClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplexClient
+{
+    public class CallbackHandler : IStockServiceCallback
+    {
+        static InstanceContext site = new InstanceContext(new CallbackHandler());
+        static StockServiceClient proxy = new StockServiceClient(site);
+        public void PriceUpdate(string ticker, double price)
+        {
+            Console.WriteLine("Получено извещение в : {0}. {1}:{2}", System.DateTime.Now, ticker, price);
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                proxy.RegisterForUpdates("MSFT");
+                Console.WriteLine("Для завершения нажмите любую клавишу");
+                Console.ReadLine();
+            }
+        }
+    }
+}
