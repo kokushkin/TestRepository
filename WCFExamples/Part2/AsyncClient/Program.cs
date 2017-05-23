a7bb1d8abc9e7480474c6b792b73d7d3c4ddd8ed
NK <fipsiksi@yandex.ru>
2017-05-23 15:42:31 +0300

    асинхронные клиенты c begin/end и async/await

--- WCFExamples/Part2/AsyncClient/Program.cs
+++ WCFExamples/Part2/AsyncClient/Program.cs
@@ -1,4 +1,4 @@
-﻿using AsyncClient.ServiceReference1;
+﻿using AsyncClient.ServiceReference2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,33 +11,63 @@ namespace AsyncClient
     class Program
     {
         static int c = 0;
+
+        //Main with begin/and (using AsyncClient.ServiceReference1;)
+
+        //static void Main(string[] args)
+        //{
+        //    StockServiceClient proxy = new StockServiceClient();
+        //    IAsyncResult arGetPrice;
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        arGetPrice = proxy.BeginGetPrice("msft", GetPriceCallback, proxy);
+        //        Interlocked.Increment(ref c);
+        //    }
+
+        //    while (c > 0)
+        //    {
+        //        Thread.Sleep(1000);
+        //        Console.WriteLine("Ожидание... Незавершенных вызовов:{0}", c);
+        //    }
+
+        //    proxy.Close();
+        //    Console.WriteLine("Готово!");
+
+        //}
+
+        //// Асинхронный обратный вызов для вывода результата.
+        //static void GetPriceCallback(IAsyncResult ar)
+        //{
+        //    double d = ((StockServiceClient)ar.AsyncState).EndGetPrice(ar);
+        //    Interlocked.Decrement(ref c);
+        //}
+
+
+        //Main with async/await (using AsyncClient.ServiceReference1;)
+
         static void Main(string[] args)
         {
             StockServiceClient proxy = new StockServiceClient();
-            IAsyncResult arGetPrice;
+
+            List<Task<double>> tasks = new List<Task<double>>();
+
             for (int i = 0; i < 10; i++)
             {
-                arGetPrice = proxy.BeginGetPrice("msft", GetPriceCallback, proxy);
-                Interlocked.Increment(ref c);
+                tasks.Add(proxy.GetPriceAsync("msft"));
             }
 
-            while (c > 0)
+
+            while(!tasks.All(tsk => tsk.IsCompleted))
             {
                 Thread.Sleep(1000);
-                Console.WriteLine("Ожидание... Незавершенных вызовов:{0}", c);
+                Console.WriteLine("Ожидание... Незавершенных вызовов");
             }
+
+            Console.WriteLine("Результаты: {0}", string.Join(";", tasks.Select(tsk => tsk.Result)));
 
             proxy.Close();
             Console.WriteLine("Готово!");
 
         }
-
-        // Асинхронный обратный вызов для вывода результата.
-        static void GetPriceCallback(IAsyncResult ar)
-        {
-            double d = ((StockServiceClient)ar.AsyncState).EndGetPrice(ar);
-            Interlocked.Decrement(ref c);
-        }
-
     }
 }
