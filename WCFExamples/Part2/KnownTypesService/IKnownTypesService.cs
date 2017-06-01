18dfa9030494b8247e1c8380c1b0ee2ec82a9b47
kokushkin <fipsiksi@yandex.ru>
2017-06-01 21:48:17 +0300

    сервис с KnownTypes. Правда из браузера он не доступен, нужно загрузить соотв. компоненты в  IIS Express(хм)

---
+++ WCFExamples/Part2/KnownTypesService/IKnownTypesService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace KnownTypesService
+{
+    [DataContract(Namespace = "http://EssentialWCF/")]
+    [KnownType(typeof(StockPrice))]
+    [KnownType(typeof(MetalPrice))]
+    public class Price
+    {
+        [DataMember]
+        public double CurrentPrice;
+        [DataMember]
+        public DateTime CurrentTime;
+        [DataMember]
+        public string Currency;
+    }
+    [DataContract(Namespace = "http://EssentialWCF/")]
+    public class StockPrice : Price
+    {
+        [DataMember]
+        public string Ticker;
+        [DataMember]
+        public long DailyVolume;
+    }
+    [DataContract(Namespace = "http://EssentialWCF/")]
+    public class MetalPrice : Price
+    {
+        [DataMember]
+        public string Metal;
+        [DataMember]
+        public string Quality;
+    }
+
+
+    [ServiceContract(Namespace = "http://EssentialWCF/FinanceService/")]
+    public interface IStockService
+    {
+        [OperationContract]
+        Price GetPrice(string id, string type);
+    }
+}
---
+++ WCFExamples/Part2/KnownTypesService/KnownTypesService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace KnownTypesService
+{
+    [ServiceBehavior(Namespace = "http://EssentialWCF/FinanceService/")]
+    public class StockService : IStockService
+    {
+
+        public Price GetPrice(string id, string type)
+        {
+            if (type.Contains("Stock"))
+            {
+                StockPrice s = new StockPrice();
+                s.Ticker = id;
+                s.DailyVolume = 45000000;
+                s.CurrentPrice = 94.15;
+                s.CurrentTime = System.DateTime.Now;
+                s.Currency = "USD";
+                return s;
+            }
+            if (type.Contains("Metal"))
+            {
+
+                MetalPrice g = new MetalPrice();
+                g.Metal = id;
+                g.Quality = "0.999";
+                g.CurrentPrice = 785.00;
+                g.CurrentTime = System.DateTime.Now;
+                g.Currency = "USD";
+                return g;
+            }
+            return new Price();
+        }
+    }
+}
