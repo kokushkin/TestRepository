057febeb657184db5676f684e8a29467bd18b37b
kokushkin <fipsiksi@yandex.ru>
2017-06-07 00:25:39 +0300

    TypedSoapService

---
+++ WCFExamples/Part3/TypedSoapService/IService1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TypedSoapService
+{
+    [Serializable]
+    public class PriceDetails
+    {
+        public string Ticker;
+        public double Amount;
+    }
+    [MessageContract]
+    public class StockPrice
+    {
+        [MessageHeader]
+        public DateTime CurrentTime;
+        [MessageBodyMember]
+        public PriceDetails Price;
+    }
+
+    [MessageContract]
+    public class StockPriceReq
+    {
+        [MessageBodyMember]
+        public string Ticker;
+    }
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        StockPrice GetPrice(StockPriceReq req);
+    }
+
+}
---
+++ WCFExamples/Part3/TypedSoapService/Service1.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TypedSoapService
+{
+    public class StockService : IStockService
+    {
+        public StockPrice GetPrice(StockPriceReq req)
+        {
+            StockPrice resp = new StockPrice();
+            resp.Price = new PriceDetails();
+            resp.Price.Ticker = req.Ticker;
+            resp.Price.Amount = 94.85;
+            return resp;
+        }
+    }
+}
