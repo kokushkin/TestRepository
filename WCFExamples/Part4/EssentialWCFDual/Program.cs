8187f1fa30376872762c4fea5878bb30025601e2
NK <fipsiksi@yandex.ru>
2017-06-10 22:42:51 +0300

    EssentialDualProj (copy)

---
+++ WCFExamples/Part4/EssentialWCFDual/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyServiceHost.StartService();
+            Console.WriteLine("To close tipe any key");
+            Console.ReadKey();
+            MyServiceHost.StopService();
+        }
+    }
+
+    internal class MyServiceHost
+    {
+        internal static ServiceHost myServiceHost = null;
+        internal static void StartService()
+        {
+            myServiceHost = new
+            ServiceHost(typeof(EssentialWCF.StockQuoteService));
+            myServiceHost.Open();
+        }
+        internal static void StopService()
+        {
+            if (myServiceHost.State != CommunicationState.Closed)
+                myServiceHost.Close();
+        }
+    }
+}
