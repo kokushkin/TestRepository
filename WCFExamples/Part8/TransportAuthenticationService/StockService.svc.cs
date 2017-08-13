e76525e5cacd2328457c74425c0ed8ee3bd54f28
kokushkin <fipsiksi@yandex.ru>
2017-08-13 21:39:30 +0300

    создал TransportAuthenticationService проект

---
+++ WCFExamples/Part8/TransportAuthenticationService/StockService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EssentialWCF
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class StockService : IStockService
+    {
+        public double GetPrice(string ticker)
+        {
+            return 94.85;
+        }
+    }
+}
