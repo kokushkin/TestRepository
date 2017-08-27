6a037ede4ad0faddd2eca6c5b2531ba1cb6a820e
kokushkin <fipsiksi@yandex.ru>
2017-08-27 18:21:48 +0300

    создал TransportAuthenticationClient и перенес ChangeContractCliet

---
+++ WCFExamples/Part8/TransportAuthenticationClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportAuthenticationClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.StockServiceClient proxy = new ServiceReference1.StockServiceClient();
+            double p = proxy.GetPrice("MSFT");
+            Console.WriteLine("Price:{0}", p);
+            proxy.Close();
+        }
+    }
+}
