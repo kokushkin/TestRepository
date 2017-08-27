47967795569f91d1bbd60f758db248a78cecf88d
kokushkin <fipsiksi@yandex.ru>
2017-08-27 18:16:30 +0300

    изменил bat'ники, добавил для создания сертификатов ка из книги. Эти самоподписанные сертификаты, норм прокатывают в firefox, но не проходят в IE и Chrom'e

--- WCFExamples/Part8/Сертификаты/CreateServerSertBook.cmd
+++ WCFExamples/Part8/Сертификаты/CreateServerSertBook.cmd
@@ -1,18 +1,13 @@
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\makecert.exe" ^
 -n "CN=myserver.com" ^
--iv CARoot.pvk ^
--ic CARoot.cer ^
+-r ^
 -pe ^
--a sha512 ^
--len 4096 ^
--b 08/26/2017 ^
--e 01/01/2018 ^
 -sky exchange ^
--eku 1.3.6.1.5.5.7.3.1 ^
 -sv %1.pvk ^
 %1.cer
- 
- 
+
+
+
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\pvk2pfx.exe" ^
 -pvk %1.pvk ^
 -spc %1.cer ^
