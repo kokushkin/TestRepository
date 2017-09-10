171edc041b7e8f0c1ccabe64c5ac37a44f69f31e
kokushkin <fipsiksi@yandex.ru>
2017-09-10 17:26:14 +0300

    аудит. сведения можно посмотреть в журнале приложений windows. неудачные поытки кажись есть в безопасности.

---
+++ WCFExamples/Part8/Audit/SelfHostedIntranetWindowsService/ISamples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleService
+{
+    [ServiceContract]
+    public interface ISamples
+    {
+        [OperationContract]     
+        string GetSecretCode();
+        [OperationContract]
+        string GetMemberCode();
+        [OperationContract]
+        string GetPublicCode();
+    }
+}
---
+++ WCFExamples/Part8/Audit/SelfHostedIntranetWindowsService/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost((typeof(Samples)));
+            serviceHost.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n"); Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
