d5c2b6d47409bb9fb94724b62bdfcceeccbc1ad4
kokushkin <fipsiksi@yandex.ru>
2017-09-10 11:45:48 +0300

    AzManAuthorization. Все ок, но админу запрещено выполнять действия пользователя. Хотя пользователь добавлен в определение админа в настройках

---
+++ WCFExamples/Part8/IntranetAuthenticationAzMan/IntranetWindowsClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntranetWindowsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для обращения к службе нажмите ENTER");
+            Console.ReadLine();
+            ServiceReference1.SamplesClient proxy = new ServiceReference1.SamplesClient("netTcp");
+
+            proxy.ClientCredentials.Windows.ClientCredential =
+                new NetworkCredential("ARTPK2\\peter", "Test123");
+            try
+            {
+                Console.WriteLine(proxy.GetPublicCode());
+                Console.WriteLine(proxy.GetMemberCode());
+                Console.WriteLine(proxy.GetSecretCode());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение = " + e.Message);
+            }
+            Console.ReadLine();
+        }
+        
+            
+    }
+}
