21dd411fbba8545f0353b880a60bbb254e35498f
kokushkin <fipsiksi@yandex.ru>
2017-09-29 11:59:39 +0300

    Part11

---
+++ WCFExamples/Part11/MEXConfigService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace EssentialWCF
+{
+    [DataContract]
+    public class StockPrice
+    {
+        [DataMember]
+        public double price;
+        [DataMember]
+        public int volume;
+    }
+
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        StockPrice GetPrice(string ticker);
+    }
+
+    public class StockService : IStockService
+    {
+        public StockPrice GetPrice(string ticker)
+        {
+            return new StockPrice { price = 4.53, volume = 23 };
+        }
+    }
+    public class Service
+    {
+        public static void Main()
+        {
+            ServiceHost serviceHost = new ServiceHost((typeof(StockService)));
+            serviceHost.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n"); Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
