3715acfe966c5ce3dc3b7849d06f673f14a7a2f5
kokushkin <fipsiksi@yandex.ru>
2018-02-19 22:21:26 +0300

    проект с поведением

---
+++ ProfWebBehavior/CustomBehaviorsLibrary/DragInCanvasBehavior.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomBehaviorsLibrary
+{
+    public class DragInCanvasBehavior : Behavior<UIElement>
+    {
+        private Canvas canvas;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // Присоединение обработчиков событий            
+            this.AssociatedObject.MouseLeftButtonDown += AssociatedObject_MouseLeftButtonDown;
+            this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
+            this.AssociatedObject.MouseLeftButtonUp += AssociatedObject_MouseLeftButtonUp;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            // Удаление обработчиков событий
+            this.AssociatedObject.MouseLeftButtonDown -= AssociatedObject_MouseLeftButtonDown;
+            this.AssociatedObject.MouseMove -= AssociatedObject_MouseMove;
+            this.AssociatedObject.MouseLeftButtonUp -= AssociatedObject_MouseLeftButtonUp;
+        }
+
+        // Отслеживание перетаскивания элемента
+        private bool isDragging = false;
+
+        // Запись точной позиции, в которой нажата кнопка
+        private Point mouseOffset;
+
+        private void AssociatedObject_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Поиск canvas
+            if (canvas == null) canvas = VisualTreeHelper.GetParent(this.AssociatedObject) as Canvas;
+
+            // Режим перетаскивания
+            isDragging = true;
+
+            // Получение позиции нажатия относительно элемента
+            mouseOffset = e.GetPosition(AssociatedObject);
+
+            // Захват мыши
+            AssociatedObject.CaptureMouse();
+        }
+
+        private void AssociatedObject_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                // Получение позиции элемента относительно Canvas
+                Point point = e.GetPosition(canvas);
+
+                // Move the element.
+                AssociatedObject.SetValue(Canvas.TopProperty, point.Y - mouseOffset.Y);
+                AssociatedObject.SetValue(Canvas.LeftProperty, point.X - mouseOffset.X);
+            }
+        }
+
+        private void AssociatedObject_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isDragging)
+            {
+                AssociatedObject.ReleaseMouseCapture();
+                isDragging = false;
+            }
+        }
+    }
+}
